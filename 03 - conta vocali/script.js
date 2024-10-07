15b65e0cab541ffa567526eed2fc49af4b98d695
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-07 14:54:17 +0200

    task_3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function counter_vocal(string) {
+    const vocali = ["a" , "e" , "i" , "o" , "u"];
+    let conteggio = 0;
+    string.split("");
+    for (let i = 0; i < string.length; i++) {
+        if (vocali.includes(string[i])){
+            conteggio++;
+        }    
+    }
+    console.log(conteggio);
+    return conteggio;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+counter_vocal(word);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
